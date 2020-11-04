390b7f7a7e6a58a400fef64946cff45ca12cd980
linwenjun <linwenjun@163.com>
2020-11-04 02:40:52 +0000

    first commit

---
+++ src/MultiplicationTable.ts
@@ -0,0 +1,5 @@
+export class MultiplicationTable {
+  public render(start: number, end: number): string{
+    return '1*1=1'
+  }
+}
---
+++ test/MultiplicationTable.test.ts
@@ -0,0 +1,16 @@
+import {MultiplicationTable} from '../src/MultiplicationTable'
+
+describe('Multiplication Table', () => {
+  it('should render multiplication table of (1, 1)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 1
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('1*1=1')
+  })
+})
