309fd637e02e3292b7716652439af059e24dc3d7
Ziyuan <zxu25@slb.com>
2020-11-17 14:40:54 +0800

    feat: add validation for input order

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,36 @@
+import { resourceLimits } from 'worker_threads'
+
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
+  public render(start: number, end: number): string {
+    if (!this.validateInputNumbers(start, end)) {
+      return ""
+    }
     return '1*1=1'
   }
+
+  private validateInputNumbers(start: number, end: number): boolean {
+    return start <= end
+  }
+
+  public generateMultipleTable(start: number, end: number): Array<Array<Expression>> {
+    const result = new Array<Array<Expression>>()
+    const side = end - start + 1
+    for (let indexRow = 0; indexRow < side; indexRow++) {
+      result.push(new Array<Expression>())
+      for (let indexColumn = 0; indexColumn < side; indexColumn++) {
+        result[indexRow].push(new Expression(start + indexColumn, start + indexRow))
+      }
+    }
+    return result
+  }
 }
+
+export class Expression {
+  public product: number
+  constructor(public factor1: number, public factor2: number) {
+    this.product = factor1 * factor2
+  }
+}
+
+// result[indexRow].push(`${numbers[0] + indexRow} * ${numbers[0] + indexRow + indexColumn}`);
+// when是指测的函数
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,4 +1,4 @@
-import {MultiplicationTable} from '../src/MultiplicationTable'
+import { MultiplicationTable } from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
   it('should render multiplication table of (1, 1)', () => {
@@ -13,4 +13,30 @@ describe('Multiplication Table', () => {
     //then
     expect(rendered).toBe('1*1=1')
   })
+
+  it('should render null of (2, 1)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe("")
+  })
+
+  // it('should render multiplication table of (1, 1)', () => {
+  //   // given
+  //   const table = new MultiplicationTable()
+  //   const start = 8
+  //   const end = 10
+
+  //   //when
+  //   const rendered = table.render(start, end)
+
+  //   //then
+  //   expect(rendered).toBe(`8*8=64\n` + `8*9=72   9*9=81\n` + `8*10=90  9*10=90  10*10=100`)
+  // })
 })
